636abb963e509df271cd2f2853be75c8c2c78555
Tejas G. <code@gtejas.com>
2024-07-23 18:14:03 +0800

    feat: add file monitoring (#10)
    * feat: add file subscription functionality
    
    * feat: add timeout for evloop

---
+++ examples/monitor.cpp
@@ -0,0 +1,30 @@
+#include "loom/backends/kqueue.hpp"
+#include "loom/fiber.hpp"
+#include "loom/utils.hpp"
+
+class Monitor : public loom::Fiber {
+public:
+    explicit Monitor(size_t stack_size) : loom::Fiber(stack_size) {}
+
+    void run() override {
+        while (true) {
+            this->wait();
+            std::cout << "File modified" << std::endl;
+        }
+    }
+};
+
+int main(int argc, char *argv[]) {
+    FLUX_ASSERT(argc == 2, "Usage: monitor <file>");
+    Monitor monitor(4096);
+    monitor.start();
+
+    loom::Kqueue kq;
+    kq.subscribe_file(argv[1], &monitor);
+
+    while (kq.active()) {
+        kq.epoch();
+    }
+
+    return 0;
+}
--- lib/include/loom/backends/kqueue.hpp
+++ lib/include/loom/backends/kqueue.hpp
@@ -10,28 +10,6 @@
 #include <string>
 #include <sys/event.h>
 
-std::string flagstring(uint32_t flags) {
-    std::ostringstream ret;
-    bool first = true;
-
-    const std::pair<int, const char *> flag_names[] = {
-        {NOTE_DELETE, "NOTE_DELETE"}, {NOTE_WRITE, "NOTE_WRITE"},
-        {NOTE_EXTEND, "NOTE_EXTEND"}, {NOTE_ATTRIB, "NOTE_ATTRIB"},
-        {NOTE_LINK, "NOTE_LINK"},     {NOTE_RENAME, "NOTE_RENAME"},
-        {NOTE_REVOKE, "NOTE_REVOKE"}};
-
-    for (const auto &[flag, name] : flag_names) {
-        if (flags & flag) {
-            if (!first) {
-                ret << "|";
-            }
-            ret << name;
-            first = false;
-        }
-    }
-
-    return ret.str();
-}
 template <>
 struct std::formatter<struct kevent> {
     constexpr auto parse(auto &ctx) { return ctx.begin(); }
@@ -40,9 +18,101 @@ struct std::formatter<struct kevent> {
         return std::format_to(ctx.out(),
                               "kevent{{ ident: {}, filter: {}, flags: {:#x}, fflags: "
                               "{}, data: {}, udata: {} }}",
-                              kev.ident, kev.filter, kev.flags, flagstring(kev.fflags),
-                              kev.data, kev.udata);
+                              kev.ident, filter_to_string(kev.filter), kev.flags,
+                              fflag_to_string(kev.fflags), kev.data, kev.udata);
     }
+
+private:
+    std::string fflag_to_string(uint32_t fflags) const {
+        std::ostringstream ret;
+        bool first = true;
+
+        for (const auto &[flag, name] : flag_names) {
+            if (fflags & flag) {
+                if (!first) {
+                    ret << "|";
+                }
+                ret << name;
+                first = false;
+            }
+        }
+
+        return ret.str();
+    }
+
+    std::string_view filter_to_string(int16_t filter) const {
+        auto it = filter_names.find(filter);
+        if (it != filter_names.end()) {
+            return it->second;
+        } else {
+            return "UNKNOWN_FILTER";
+        }
+    }
+
+    inline static const std::unordered_map<int16_t, const char *> filter_names = {
+        {EVFILT_READ, "EVFILT_READ"},
+        {EVFILT_WRITE, "EVFILT_WRITE"},
+        {EVFILT_AIO, "EVFILT_AIO"},
+        {EVFILT_VNODE, "EVFILT_VNODE"},
+        {EVFILT_PROC, "EVFILT_PROC"},
+        {EVFILT_SIGNAL, "EVFILT_SIGNAL"},
+        {EVFILT_TIMER, "EVFILT_TIMER"},
+        {EVFILT_MACHPORT, "EVFILT_MACHPORT"},
+        {EVFILT_FS, "EVFILT_FS"},
+        {EVFILT_USER, "EVFILT_USER"},
+        {EVFILT_VM, "EVFILT_VM"},
+        {EVFILT_EXCEPT, "EVFILT_EXCEPT"},
+        {EVFILT_SYSCOUNT, "EVFILT_SYSCOUNT"},
+        {EVFILT_THREADMARKER, "EVFILT_THREADMARKER"}};
+
+    constexpr static std::pair<uint32_t, const char *> flag_names[] = {
+        {NOTE_TRIGGER, "NOTE_TRIGGER"},
+        {NOTE_FFNOP, "NOTE_FFNOP"},
+        {NOTE_FFAND, "NOTE_FFAND"},
+        {NOTE_FFOR, "NOTE_FFOR"},
+        {NOTE_FFCOPY, "NOTE_FFCOPY"},
+        {NOTE_FFCTRLMASK, "NOTE_FFCTRLMASK"},
+        {NOTE_FFLAGSMASK, "NOTE_FFLAGSMASK"},
+        {NOTE_LOWAT, "NOTE_LOWAT"},
+        {NOTE_OOB, "NOTE_OOB"},
+        {NOTE_DELETE, "NOTE_DELETE"},
+        {NOTE_WRITE, "NOTE_WRITE"},
+        {NOTE_EXTEND, "NOTE_EXTEND"},
+        {NOTE_ATTRIB, "NOTE_ATTRIB"},
+        {NOTE_LINK, "NOTE_LINK"},
+        {NOTE_RENAME, "NOTE_RENAME"},
+        {NOTE_REVOKE, "NOTE_REVOKE"},
+        {NOTE_NONE, "NOTE_NONE"},
+        {NOTE_FUNLOCK, "NOTE_FUNLOCK"},
+        {NOTE_LEASE_DOWNGRADE, "NOTE_LEASE_DOWNGRADE"},
+        {NOTE_LEASE_RELEASE, "NOTE_LEASE_RELEASE"},
+        {NOTE_EXIT, "NOTE_EXIT"},
+        {NOTE_FORK, "NOTE_FORK"},
+        {NOTE_EXEC, "NOTE_EXEC"},
+        {NOTE_SIGNAL, "NOTE_SIGNAL"},
+        {NOTE_EXITSTATUS, "NOTE_EXITSTATUS"},
+        {NOTE_EXIT_DETAIL, "NOTE_EXIT_DETAIL"},
+        {NOTE_PDATAMASK, "NOTE_PDATAMASK"},
+        {NOTE_EXIT_DETAIL_MASK, "NOTE_EXIT_DETAIL_MASK"},
+        {NOTE_EXIT_DECRYPTFAIL, "NOTE_EXIT_DECRYPTFAIL"},
+        {NOTE_EXIT_MEMORY, "NOTE_EXIT_MEMORY"},
+        {NOTE_EXIT_CSERROR, "NOTE_EXIT_CSERROR"},
+        {NOTE_VM_PRESSURE, "NOTE_VM_PRESSURE"},
+        {NOTE_VM_PRESSURE_TERMINATE, "NOTE_VM_PRESSURE_TERMINATE"},
+        {NOTE_VM_PRESSURE_SUDDEN_TERMINATE, "NOTE_VM_PRESSURE_SUDDEN_TERMINATE"},
+        {NOTE_VM_ERROR, "NOTE_VM_ERROR"},
+        {NOTE_SECONDS, "NOTE_SECONDS"},
+        {NOTE_USECONDS, "NOTE_USECONDS"},
+        {NOTE_NSECONDS, "NOTE_NSECONDS"},
+        {NOTE_ABSOLUTE, "NOTE_ABSOLUTE"},
+        {NOTE_LEEWAY, "NOTE_LEEWAY"},
+        {NOTE_CRITICAL, "NOTE_CRITICAL"},
+        {NOTE_BACKGROUND, "NOTE_BACKGROUND"},
+        {NOTE_MACH_CONTINUOUS_TIME, "NOTE_MACH_CONTINUOUS_TIME"},
+        {NOTE_MACHTIME, "NOTE_MACHTIME"},
+        {NOTE_TRACK, "NOTE_TRACK"},
+        {NOTE_TRACKERR, "NOTE_TRACKERR"},
+        {NOTE_CHILD, "NOTE_CHILD"}};
 };
 
 namespace loom {
@@ -53,12 +123,14 @@ public:
     int m_kq_fd;
     std::array<struct kevent, 16> m_changes;
     int m_nchanges;
-    struct timespec *m_timeout = nullptr;
+    struct timespec m_timeout {};
 
 public:
     Kqueue() {
         m_kq_fd = kqueue();
         m_nchanges = 0;
+        m_timeout.tv_sec = 0;
+        m_timeout.tv_nsec = 500000000;
         FLUX_ASSERT(m_kq_fd != -1, "Failed to create kqueue");
     }
 
@@ -82,7 +154,7 @@ public:
         // `m_timeout` must be non-empty if we want to also monitor SPSC queues for
         // example `m_timeout` == 0 degrades the performance to a regular poll event loop
         int n = kevent(m_kq_fd, m_changes.data(), m_nchanges, events.data(),
-                       events.size(), m_timeout);
+                       events.size(), &m_timeout);
         m_nchanges = 0;
 
         // `kevent` can be interrupted by signals, so we check `errno` global variable.
@@ -91,13 +163,14 @@ public:
 
         for (int i = 0; i < n; ++i) {
 #ifndef NDEBUG
-            std::cout << std::format("Event: {}\n", events[i]);
+            std::cout << std::format("{}\n", events[i]);
 #endif
             if (events[i].flags & EV_ERROR) {
                 printf("Error event\n");
                 continue;
             }
             if (events[i].filter == EVFILT_READ) {
+                // Not just socket reads but reads for files too
                 this->notify(Event{.type = Event::Type::SocketRead,
                                    .fd = static_cast<int>(events[i].ident)});
             } else if (events[i].filter == EVFILT_WRITE) {
@@ -106,8 +179,18 @@ public:
             } else if (events[i].filter == EVFILT_TIMER) {
                 this->notify(Event{.type = Event::Type::Timer,
                                    .fd = static_cast<int>(events[i].ident)});
+            } else if (events[i].filter == EVFILT_VNODE) {
+                this->notify(Event{.type = Event::Type::FileModified,
+                                   .fd = static_cast<int>(events[i].ident)});
+            } else {
+                printf("Unknown event\n");
             }
         }
+
+        // Reset the timeout. In case of a signal interruption the values might change
+        m_timeout.tv_sec = 0;
+        m_timeout.tv_nsec = 500000000;
+
         return true;
     }
 
@@ -136,10 +219,8 @@ public:
         FLUX_ASSERT(m_kq_fd >= 0, "Kqueue file descriptor is invalid");
 #endif
 
-        throw new std::runtime_error("Not implemented");
         if (m_nchanges >= 10) [[unlikely]] {
             // `changelist` is full, need to flush the changes to the kernel.
-            printf("This shouldnt print\n");
             int n = kevent(m_kq_fd, m_changes.data(), m_nchanges, nullptr, 0, nullptr);
             // abstract into a "flush" command
             FLUX_ASSERT(n >= 0, "Failed to flush changes to the kernel");
@@ -159,6 +240,14 @@ public:
         case Operation::Removed:
             EV_SET(&m_changes[m_nchanges++], fd, EVFILT_READ, EV_DELETE, 0, 0, nullptr);
             EV_SET(&m_changes[m_nchanges++], fd, EVFILT_WRITE, EV_DELETE, 0, 0, nullptr);
+            break;
+        case Operation::File:
+            EV_SET(&m_changes[m_nchanges++], fd, EVFILT_VNODE, EV_ADD | EV_CLEAR,
+                   NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK |
+                       NOTE_RENAME | NOTE_REVOKE,
+                   0, nullptr);
+            EV_SET(&m_changes[m_nchanges++], fd, EVFILT_READ, EV_ADD | EV_CLEAR, 0, 0,
+                   nullptr);
             break;
         case Operation::Modified: [[fallthrough]];
         case Operation::NIL: break;
